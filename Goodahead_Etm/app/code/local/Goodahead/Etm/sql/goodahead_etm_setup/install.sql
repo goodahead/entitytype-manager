751a4b6cc2293db5a0a464b27f9ff64fb5125da3
anjey-lobas <anjey.lobas@goodahead.com>
2013-06-01 20:09:42 +0300

    Changed eav to entity in table names Added linked field to catalog_eav_attribute

--- Goodahead_Etm/app/code/local/Goodahead/Etm/sql/goodahead_etm_setup/install.sql
+++ Goodahead_Etm/app/code/local/Goodahead/Etm/sql/goodahead_etm_setup/install.sql
@@ -1,4 +1,7 @@
 DELETE FROM `core_resource` WHERE code = 'goodahead_etm_setup';
+ALTER TABLE catalog_eav_attribute DROP FOREIGN KEY FK_4C54C2F21602E7FD345B67E0BDCF6658;
+ALTER TABLE catalog_eav_attribute DROP `goodahead_etm_entity_type_id`;
+
 DROP TABLE IF EXISTS `goodahead_etm_eav_datetime`;
 DROP TABLE IF EXISTS `goodahead_etm_eav_decimal`;
 DROP TABLE IF EXISTS `goodahead_etm_eav_int`;
@@ -7,3 +10,12 @@ DROP TABLE IF EXISTS `goodahead_etm_eav_varchar`;
 DROP TABLE IF EXISTS `goodahead_etm_eav_char`;
 DROP TABLE IF EXISTS `goodahead_etm_eav_entity_type`;
 DROP TABLE IF EXISTS `goodahead_etm_eav`;
+
+DROP TABLE IF EXISTS `goodahead_etm_entity_datetime`;
+DROP TABLE IF EXISTS `goodahead_etm_entity_decimal`;
+DROP TABLE IF EXISTS `goodahead_etm_entity_int`;
+DROP TABLE IF EXISTS `goodahead_etm_entity_text`;
+DROP TABLE IF EXISTS `goodahead_etm_entity_varchar`;
+DROP TABLE IF EXISTS `goodahead_etm_entity_char`;
+DROP TABLE IF EXISTS `goodahead_etm_entity_entity_type`;
+DROP TABLE IF EXISTS `goodahead_etm_entity`;
