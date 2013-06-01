f07e322150707ddfef84544f7ac7df48263109a0
Roman Lesyk <roma.lesyk@gmail.com>
2013-06-01 18:32:55 +0200

    Revert dummy code

--- Goodahead_Etm/app/code/local/Goodahead/Etm/controllers/IndexController.php
+++ Goodahead_Etm/app/code/local/Goodahead/Etm/controllers/IndexController.php
@@ -2,19 +2,4 @@
 
 class Goodahead_Etm_IndexController extends Mage_Core_Controller_Front_Action
 {
-    public function indexAction()
-    {
-        $entityType = Mage::getModel('goodahead_etm/entity_type');
-        $entityType->load(33);
-        var_dump($entityType->getId());
-        return;
-        $model = Mage::getModel('goodahead_etm/entity_type');
-        $model->load(1);
-        var_dump($model->getId());
-        return;
-        $collection = Mage::getModel('goodahead_etm/entity_type')->getCollection();
-        $collection->load();
-        echo get_class($collection);
-        echo count($collection);
-    }
 }
