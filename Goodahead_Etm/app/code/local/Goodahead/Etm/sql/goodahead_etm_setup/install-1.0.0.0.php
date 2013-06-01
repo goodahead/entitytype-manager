f97c9079a7cdd1e277c9edbaeffc3bd8aecd87f4
Igor Vitol <igor.vitol@goodahead.com>
2013-06-01 13:22:52 +0300

    Base install script

--- Goodahead_Etm/app/code/local/Goodahead/Etm/sql/goodahead_etm_setup/install-1.0.0.0.php
+++ Goodahead_Etm/app/code/local/Goodahead/Etm/sql/goodahead_etm_setup/install-1.0.0.0.php
@@ -1,9 +1,19 @@
 <?php
 
-/** @var $installer Mage_Eav_Model_Entity_Setup */
+/** @var $installer Goodahead_Etm_Model_Entity_Setup */
 $installer = $this;
 $installer->startSetup();
 
-// put install code here
+$installer->run("
+DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}`;
+DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_datetime`;
+DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_decimal`;
+DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_int`;
+DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_text`;
+DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_varchar`;
+DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_char`;
+");
+
+$installer->createEntityTables('goodahead_etm/eav');
 
 $installer->endSetup();
