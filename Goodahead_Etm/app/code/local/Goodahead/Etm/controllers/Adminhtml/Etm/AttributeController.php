c573755b2d37bd7aa8f536747b1b8049730b6f0d
Dmitriy Zavalkin <dmitriy.zavalkin@gmail.com>
2013-06-01 14:21:38 +0200

    - added menu for all admin controllers - fixed acl

--- Goodahead_Etm/app/code/local/Goodahead/Etm/controllers/Adminhtml/Etm/AttributeController.php
+++ Goodahead_Etm/app/code/local/Goodahead/Etm/controllers/Adminhtml/Etm/AttributeController.php
@@ -1,6 +1,6 @@
 <?php
 
-class Goodahead_Etm_Adminhtml_Etm_AttributeController extends Goodahead_Etm_Controllers_Adminhtml
+class Goodahead_Etm_Adminhtml_Etm_AttributeController extends Goodahead_Etm_Controller_Adminhtml
 {
     /**
      * Entity Type Manager index page
@@ -9,21 +9,12 @@ class Goodahead_Etm_Adminhtml_Etm_AttributeController extends Goodahead_Etm_Cont
     {
         try {
             $this->_initEntityType();
-            $this->loadLayout();
-            $this->_setActiveMenu('goodahead_etm');
-            $this->_addBreadcrumb(
-                Mage::helper('goodahead_etm')->__('Entity Type Manager'),
-                Mage::helper('goodahead_etm')->__('Entity Type Manager')
-            );
-            $this->_addBreadcrumb(
-                Mage::helper('goodahead_etm')->__('Manage Attributes'),
-                Mage::helper('goodahead_etm')->__('Manage Attributes')
-            );
 
-
+            $this->_initAction($this->__('Manage Attributes'));
             $this->renderLayout();
             // TODO: Catch only our exception
         } catch (Exception $e) {
+            Mage::logException($e);
             $this->_forward('no_route');
         }
     }
