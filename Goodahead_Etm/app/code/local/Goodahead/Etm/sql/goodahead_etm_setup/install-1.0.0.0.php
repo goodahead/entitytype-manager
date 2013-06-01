643c26d2d2f9ce026b4241e836269dabefb20c3e
Igor Vitol <igor.vitol@goodahead.com>
2013-06-01 13:31:23 +0300

    Updated sql install script

--- Goodahead_Etm/app/code/local/Goodahead/Etm/sql/goodahead_etm_setup/install-1.0.0.0.php
+++ Goodahead_Etm/app/code/local/Goodahead/Etm/sql/goodahead_etm_setup/install-1.0.0.0.php
@@ -4,16 +4,6 @@
 $installer = $this;
 $installer->startSetup();
 
-$installer->run("
-DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}`;
-DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_datetime`;
-DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_decimal`;
-DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_int`;
-DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_text`;
-DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_varchar`;
-DROP TABLE IF EXISTS `{$this->getTable('goodahead_etm/eav')}_char`;
-");
-
 $installer->createEntityTables('goodahead_etm/eav');
 
 $installer->endSetup();
---
+++ Goodahead_Etm/app/code/local/Goodahead/Etm/sql/goodahead_etm_setup/install.sql
@@ -0,0 +1,8 @@
+DELETE FROM `core_resource` WHERE code = 'goodahead_etm_setup';
+DROP TABLE IF EXISTS `goodahead_etm_eav`;
+DROP TABLE IF EXISTS `goodahead_etm_eav_datetime`;
+DROP TABLE IF EXISTS `goodahead_etm_eav_decimal`;
+DROP TABLE IF EXISTS `goodahead_etm_eav_int`;
+DROP TABLE IF EXISTS `goodahead_etm_eav_text`;
+DROP TABLE IF EXISTS `goodahead_etm_eav_varchar`;
+DROP TABLE IF EXISTS `goodahead_etm_eav_char`;
