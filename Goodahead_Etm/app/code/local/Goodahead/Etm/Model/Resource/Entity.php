399cdb10415af90e65d06d5b0370a112e5725a81
Roman Lesyk <roma.lesyk@gmail.com>
2013-06-02 00:15:07 +0200

    Implement load of entity model by id

--- Goodahead_Etm/app/code/local/Goodahead/Etm/Model/Resource/Entity.php
+++ Goodahead_Etm/app/code/local/Goodahead/Etm/Model/Resource/Entity.php
@@ -7,7 +7,7 @@ class Goodahead_Etm_Model_Resource_Entity extends Mage_Eav_Model_Entity_Abstract
     public function __construct($entityTypeCode)
     {
         parent::__construct();
-        $this->setType($entityTypeCode);
+        $this->setType($entityTypeCode['entity_type_id']);
         $this->setConnection('eav_read', 'eav_write');
     }
 }
