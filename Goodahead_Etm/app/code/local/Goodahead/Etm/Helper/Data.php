92a65892bc374e7109921eeea77b9ed002847091
Dmitriy Zavalkin <dmitriy.zavalkin@gmail.com>
2013-06-01 10:56:27 +0200

    created module skeleton

---
+++ Goodahead_Etm/app/code/local/Goodahead/Etm/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+
+class Goodahead_Etm_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
---
+++ Goodahead_Etm/app/code/local/Goodahead/Etm/sql/goodahead_etm_setup/install-1.0.0.0.php
@@ -0,0 +1,9 @@
+<?php
+
+/** @var $installer Mage_Eav_Model_Entity_Setup */
+$installer = $this;
+$installer->startSetup();
+
+// put install code here
+
+$installer->endSetup();
